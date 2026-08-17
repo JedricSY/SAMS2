f0c87623e14ba891e35fe427b74dc3badb263771
JedricSY <jericho.mauricio@deped.gov.ph>
2026-08-17 14:49:57 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Paste the "Web app URL" you get after deploying the Apps Script backend
 // (Deploy > New deployment > Web app). It looks like:
 // https://script.google.com/macros/s/AKfycbx.../exec
-const API_BASE_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbx60ShHyAYyfsNbM-5dE8s6ygGoWDFficv-3Jgu7CxqvmyWylQCaImjjYYimJLq-VoggQ/exec';
